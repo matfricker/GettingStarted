4a282c3f5d1effd63e174b00793ec558aa5bd1ac
Matt Fricker <matt@frikz.co>
2021-09-09 05:38:46 +0100

    guess a randomly generated number between 1 & 10

--- 09-guessing-game.py
+++ 09-guessing-game.py
@@ -1,13 +1,28 @@
-secret_word = "banjo"
-guess = ""
-guess_count = 0
-guess_limit = 3
+import random
+
+secret_num = random.randint(1,10)
+
+guess = 0
+count = 0
+limit = 10
+remaining_guesses = 0
 out_of_guesses = False
 
-while guess != secret_word and not(out_of_guesses):
-    if guess_count < guess_limit:
-        guess = input("Enter your guess:\n")
-        guess_count += 1
+def checkNum():
+    if guess > secret_num:
+        print("Too high, " + str(remaining_guesses) + " left")
+    elif guess < secret_num:
+        print("Too low, " + str(remaining_guesses) + " left")
+
+print("Pick a number between 1 and 10.")
+print("You have " + str(limit) + " gueses.")
+
+while guess != secret_num and not(out_of_guesses):
+    if count < limit:
+        guess = int(input("Enter your guess:\n"))
+        count += 1
+        remaining_guesses = limit - count
+        checkNum()
     else:
         out_of_guesses = True
 
